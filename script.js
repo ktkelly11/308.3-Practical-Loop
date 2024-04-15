b9ab532541ce0fa8e6212c68a1dc15a9711bd321
Katherine Kelly <ktkelly11@gmail.com>
2024-04-15 12:30:26 -0400

    Prime Time added back in

--- script.js
+++ script.js
@@ -18,17 +18,20 @@ for (let a = 1; a <= 100; a++) {
 // 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11,...
 
 // Part 2: Prime Time
-for (let n = 9; n <= 100; n++) {
+for (let n = 14; n <= 100; n++) {
   let prime = true;
   for (let p = 2; p < n; p++) {
     if (n % p == 0) {
       prime = false;
-      p = n;
+      p = n + 1;
       break;
     }
   }
   if (prime && n != 1) {
-    console.log(`${n} is a prime number`);
+    console.log(`${n} is the next prime number`);
     break;
   }
 }
+// 17 is the next prime number
+
+// Part 3: Feeling Loopy
