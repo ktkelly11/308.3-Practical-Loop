07c84172d9074acef421e8f814f0852d267e598d
Katherine Kelly <ktkelly11@gmail.com>
2024-04-15 13:06:57 -0400

    first attempt at Feeling Loopy

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for (let a = 1; a <= 100; a++) {
 // 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11,...
 
 // Part 2: Prime Time
-for (let n = 14; n <= 100; n++) {
+for (let n = 9; n + 1 <= 100; n++) {
   let prime = true;
   for (let p = 2; p < n; p++) {
     if (n % p == 0) {
@@ -27,11 +27,30 @@ for (let n = 14; n <= 100; n++) {
       break;
     }
   }
-  if (prime && n != 1) {
+  if (prime && n + 1 != 1) {
     console.log(`${n} is the next prime number`);
     break;
   }
 }
-// 17 is the next prime number
+// 11 is the next prime number
 
 // Part 3: Feeling Loopy
+const myString = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+console.log(myString);
+
+// First Attempt
+// const myArray = {
+//   id: ("ID", "42", "57", "63", "98"),
+//   name: ("Name", "Bruce", "Bob", "Blaine", "Bill"),
+//   occupation:
+//     ("Occupation", "Knight", "Fry Cook", "Quiz Master", "Doctor's Assitant"),
+//   age: ("Age", "41", "19", "58", "26"),
+// };
+
+// let strArray = myArray(3);
+
+// for (i=0; i<= myArray.length; i++) {
+//   if (strArray[i] != null)
+// }
+
+// Second Attempt
